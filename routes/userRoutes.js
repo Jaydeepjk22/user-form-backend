a4dfd76cc4eacd1f1d50bd923bcbfdc566b0eacf
Jayadeep JK <jayadeep@zeliot.in>
2023-06-16 22:43:39 +0530

    package installed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
-import express from "express";
-import { createUser, getUsers } from "../controllers/userController";
+const express = require("express");
+const { createUser, getUsers } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/", createUser);
 router.get("/", getUsers);
 
-export default router;
+module.exports = router;
